3b8badc2b99f64bde85ded4e7e8b1880526cdf63
Himu004 <himu.chowdhury01@gmail.com>
2024-12-24 15:55:29 +0600

    Mongo DB Innitiated

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require("express");
 const cors = require("cors");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 
 
-app.listen(port, () => {
-  console.log(`Eleven Lab Restaurant Backend listening on port ${port}`);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.30scn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    console.log('Connected to MONGODB successfully');
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get("/", (req, res) => {
+    res.send("Eleven Lab Restaurant Server is running....");
+  });
+  
+  app.listen(port, () => console.log(`Server running on port ${port}`));
