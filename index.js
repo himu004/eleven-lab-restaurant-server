cf479cd9801001c537f05c29516b3cbe3add719e
Himu004 <himu.chowdhury01@gmail.com>
2024-12-25 16:32:32 +0600

    Food Purchase Api Created with Qunatity Validations and increase purchase count

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-
 const port = process.env.PORT || 3000;
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.30scn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,82 +17,146 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    console.log('Connected to MONGODB successfully');
+    console.log("Connected to MONGODB successfully");
 
     // MY DATABASE
     const db = client.db("eleven-lab-restaurant");
     const foodsCollection = db.collection("foods");
+    const purchaseCollection = db.collection("purchase");
 
     // Add Foods Api
-    app.post('/add-food', async (req, res) => {
+    app.post("/add-food", async (req, res) => {
       const food = req.body;
       const result = await foodsCollection.insertOne(food);
       res.send(result);
     });
 
     // Get All Foods Api
-    app.get('/all-foods', async (req, res) => {
+    app.get("/all-foods", async (req, res) => {
       const result = await foodsCollection.find({}).toArray();
       res.send(result);
     });
 
     // Get Single Food Api
-    app.get('/food/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await foodsCollection.findOne(query);
-        res.send(result);
-      });
-    
+    app.get("/food/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodsCollection.findOne(query);
+      res.send(result);
+    });
+
     // API FOR ONLY 6 FOODS
-    app.get('/top-foods', async (req, res) => {
-        // const query = { purchase_count: { $gt: 0 } };
-        const result = await foodsCollection.find({}).limit(6).toArray();
-        res.send(result);
-      });
+    app.get("/top-foods", async (req, res) => {
+      const query = { purchase_count: { $gt: 0 } };
+      const result = await foodsCollection.find(query).limit(6).toArray();
+      res.send(result);
+    });
 
     //   Implement Search Api
-    app.get('/search', async (req, res) => {
-        const search = req.query.search || "";
-        const query = { name: { $regex: search.toString(), $options: "i" } };
-        const result = await foodsCollection.find(query).toArray();
-        res.send(result);
-      });
+    app.get("/search", async (req, res) => {
+      const search = req.query.search || "";
+      const query = { name: { $regex: search.toString(), $options: "i" } };
+      const result = await foodsCollection.find(query).toArray();
+      res.send(result);
+    });
 
     //   My Foods Api Filtered by Email
-    app.get('/my-foods/:email', async (req, res) => {
-        const email = req.params.email;
-        const query = { 'addedBy.email': email };
-        const result = await foodsCollection.find(query).toArray();
-        res.send(result);
-      });
+    app.get("/my-foods/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "addedBy.email": email };
+      const result = await foodsCollection.find(query).toArray();
+      res.send(result);
+    });
 
     //  Update Foods Api
-    app.put('/update-food/:id', async (req, res) => {
-        const id = req.params.id;
-        const food = req.body;
-        const query = { _id: new ObjectId(id) };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: {
-            name: food.name,
-            price: food.price,
-            category: food.category,
-            image: food.image,
-            description: food.description,
-          },
-        };
-        const result = await foodsCollection.updateOne(query, updateDoc, options);
-        res.send(result);
-    }) 
+    app.put("/update-food/:id", async (req, res) => {
+      const id = req.params.id;
+      const food = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: food.name,
+          price: food.price,
+          category: food.category,
+          image: food.image,
+          description: food.description,
+        },
+      };
+      const result = await foodsCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
 
+    // Foood Purchase APi
+    app.post("/food-purchase", async (req, res) => {
+      const purchase = req.body;
+      const { foodId, quantity } = purchase;
+        const food = await foodsCollection.findOne({ _id: new ObjectId(foodId) });
+      // Parse quantity to float
+      const parsedQuantity = parseFloat(quantity);
 
+      // Check if the food quantity is available
+      if (food.quantity <= 0 || food.quantity < parsedQuantity) {
+        return res.status(400).json({ message: "Insufficient food quantity" });
+      }
+      const result = await purchaseCollection.insertOne(purchase);
 
+      // Food Data Increment Purchase Count
+      const foodData = req.body;
+
+      // 2. Increase the bid count in the job collection
+      const filter = { _id: new ObjectId(foodData.foodId) };
+      const update = {
+        $inc: { purchase_count: 1 },
+      };
+      const updateBidCount = await foodsCollection.updateOne(filter, update);
+      res.send(result);
+    });
+
+    // Food Purchase Api
+    // app.post('/food-purchase', async (req, res) => {
+    //     const purchase = req.body;
+    //     const { foodId, userEmail, quantity } = purchase;
+
+    //     // Parse quantity to float
+    //     const parsedQuantity = parseFloat(quantity);
+    //     if (isNaN(parsedQuantity)) {
+    //         return res.status(400).send({ message: "Invalid quantity" });
+    //     }
+
+    //     // Find the food item
+    //     const food = await foodsCollection.findOne({ _id: new ObjectId(foodId) });
+
+    //     if (!food) {
+    //         return res.status(404).send({ message: "Food item not found" });
+    //     }
+
+    //     // Check if the user who added the food is trying to purchase it
+    //     if (food.addedBy.email === userEmail) {
+    //         return res.status(400).send({ message: "You cannot purchase your own food item" });
+    //     }
+
+    //     // Check if the food quantity is available
+    //     if (food.quantity <= 0 || food.quantity < parsedQuantity) {
+    //         return res.status(400).send({ message: "Insufficient food quantity" });
+    //     }
+
+    //     // Update the food's purchase count and quantity
+    //     await foodsCollection.updateOne(
+    //         { _id: new ObjectId(foodId) },
+    //         { $inc: { purchase_count: parsedQuantity, quantity: -parsedQuantity } }
+    //     );
+
+    //     // Insert the purchase record
+    //     const result = await purchaseCollection.insertOne(purchase);
+    //     res.send(result);
+    //     // console.log(result);
+    // });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -105,10 +164,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("Eleven Lab Restaurant Server is running....");
-  });
-  
-  app.listen(port, () => console.log(`Server running on port ${port}`));
+  res.send("Eleven Lab Restaurant Server is running....");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
